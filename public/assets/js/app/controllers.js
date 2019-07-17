2fe7c59637c119b88815f05bd65861b104ffb3bd
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-07-16 20:20:51 -0400

    Added additional style and map to map page

--- public/assets/js/app/controllers.js
+++ public/assets/js/app/controllers.js
@@ -1,57 +1,306 @@
 angular.module('Application.Controllers', [])
 
-.controller("RichlistController", ["$scope", "$timeout", function($scope, $timeout) {
-      $scope.richlist = {};
-      $.get("/api/richlist")
-        .then(function(data) {
-            $timeout(function() {
-                $scope.richlist = data.richlist;
-            })
-        });
-}])
+.controller("LandingController", ["$scope", "$timeout", function($scope, $timeout) {
 
-.controller("ShirtController", ["$scope", "$timeout", function($scope, $timeout) {
-}])
+        var isoCountries = {
+            'AF' : 'Afghanistan',
+            'AX' : 'Aland Islands',
+            'AL' : 'Albania',
+            'DZ' : 'Algeria',
+            'AS' : 'American Samoa',
+            'AD' : 'Andorra',
+            'AO' : 'Angola',
+            'AI' : 'Anguilla',
+            'AQ' : 'Antarctica',
+            'AG' : 'Antigua And Barbuda',
+            'AR' : 'Argentina',
+            'AM' : 'Armenia',
+            'AW' : 'Aruba',
+            'AU' : 'Australia',
+            'AT' : 'Austria',
+            'AZ' : 'Azerbaijan',
+            'BS' : 'Bahamas',
+            'BH' : 'Bahrain',
+            'BD' : 'Bangladesh',
+            'BB' : 'Barbados',
+            'BY' : 'Belarus',
+            'BE' : 'Belgium',
+            'BZ' : 'Belize',
+            'BJ' : 'Benin',
+            'BM' : 'Bermuda',
+            'BT' : 'Bhutan',
+            'BO' : 'Bolivia',
+            'BA' : 'Bosnia And Herzegovina',
+            'BW' : 'Botswana',
+            'BV' : 'Bouvet Island',
+            'BR' : 'Brazil',
+            'IO' : 'British Indian Ocean Territory',
+            'BN' : 'Brunei Darussalam',
+            'BG' : 'Bulgaria',
+            'BF' : 'Burkina Faso',
+            'BI' : 'Burundi',
+            'KH' : 'Cambodia',
+            'CM' : 'Cameroon',
+            'CA' : 'Canada',
+            'CV' : 'Cape Verde',
+            'KY' : 'Cayman Islands',
+            'CF' : 'Central African Republic',
+            'TD' : 'Chad',
+            'CL' : 'Chile',
+            'CN' : 'China',
+            'CX' : 'Christmas Island',
+            'CC' : 'Cocos (Keeling) Islands',
+            'CO' : 'Colombia',
+            'KM' : 'Comoros',
+            'CG' : 'Congo',
+            'CD' : 'Congo, Democratic Republic',
+            'CK' : 'Cook Islands',
+            'CR' : 'Costa Rica',
+            'CI' : 'Cote D\'Ivoire',
+            'HR' : 'Croatia',
+            'CU' : 'Cuba',
+            'CY' : 'Cyprus',
+            'CZ' : 'Czech Republic',
+            'DK' : 'Denmark',
+            'DJ' : 'Djibouti',
+            'DM' : 'Dominica',
+            'DO' : 'Dominican Republic',
+            'EC' : 'Ecuador',
+            'EG' : 'Egypt',
+            'SV' : 'El Salvador',
+            'GQ' : 'Equatorial Guinea',
+            'ER' : 'Eritrea',
+            'EE' : 'Estonia',
+            'ET' : 'Ethiopia',
+            'FK' : 'Falkland Islands (Malvinas)',
+            'FO' : 'Faroe Islands',
+            'FJ' : 'Fiji',
+            'FI' : 'Finland',
+            'FR' : 'France',
+            'GF' : 'French Guiana',
+            'PF' : 'French Polynesia',
+            'TF' : 'French Southern Territories',
+            'GA' : 'Gabon',
+            'GM' : 'Gambia',
+            'GE' : 'Georgia',
+            'DE' : 'Germany',
+            'GH' : 'Ghana',
+            'GI' : 'Gibraltar',
+            'GR' : 'Greece',
+            'GL' : 'Greenland',
+            'GD' : 'Grenada',
+            'GP' : 'Guadeloupe',
+            'GU' : 'Guam',
+            'GT' : 'Guatemala',
+            'GG' : 'Guernsey',
+            'GN' : 'Guinea',
+            'GW' : 'Guinea-Bissau',
+            'GY' : 'Guyana',
+            'HT' : 'Haiti',
+            'HM' : 'Heard Island & Mcdonald Islands',
+            'VA' : 'Holy See (Vatican City State)',
+            'HN' : 'Honduras',
+            'HK' : 'Hong Kong',
+            'HU' : 'Hungary',
+            'IS' : 'Iceland',
+            'IN' : 'India',
+            'ID' : 'Indonesia',
+            'IR' : 'Iran, Islamic Republic Of',
+            'IQ' : 'Iraq',
+            'IE' : 'Ireland',
+            'IM' : 'Isle Of Man',
+            'IL' : 'Israel',
+            'IT' : 'Italy',
+            'JM' : 'Jamaica',
+            'JP' : 'Japan',
+            'JE' : 'Jersey',
+            'JO' : 'Jordan',
+            'KZ' : 'Kazakhstan',
+            'KE' : 'Kenya',
+            'KI' : 'Kiribati',
+            'KR' : 'Korea',
+            'KW' : 'Kuwait',
+            'KG' : 'Kyrgyzstan',
+            'LA' : 'Lao People\'s Democratic Republic',
+            'LV' : 'Latvia',
+            'LB' : 'Lebanon',
+            'LS' : 'Lesotho',
+            'LR' : 'Liberia',
+            'LY' : 'Libyan Arab Jamahiriya',
+            'LI' : 'Liechtenstein',
+            'LT' : 'Lithuania',
+            'LU' : 'Luxembourg',
+            'MO' : 'Macao',
+            'MK' : 'Macedonia',
+            'MG' : 'Madagascar',
+            'MW' : 'Malawi',
+            'MY' : 'Malaysia',
+            'MV' : 'Maldives',
+            'ML' : 'Mali',
+            'MT' : 'Malta',
+            'MH' : 'Marshall Islands',
+            'MQ' : 'Martinique',
+            'MR' : 'Mauritania',
+            'MU' : 'Mauritius',
+            'YT' : 'Mayotte',
+            'MX' : 'Mexico',
+            'FM' : 'Micronesia, Federated States Of',
+            'MD' : 'Moldova',
+            'MC' : 'Monaco',
+            'MN' : 'Mongolia',
+            'ME' : 'Montenegro',
+            'MS' : 'Montserrat',
+            'MA' : 'Morocco',
+            'MZ' : 'Mozambique',
+            'MM' : 'Myanmar',
+            'NA' : 'Namibia',
+            'NR' : 'Nauru',
+            'NP' : 'Nepal',
+            'NL' : 'Netherlands',
+            'AN' : 'Netherlands Antilles',
+            'NC' : 'New Caledonia',
+            'NZ' : 'New Zealand',
+            'NI' : 'Nicaragua',
+            'NE' : 'Niger',
+            'NG' : 'Nigeria',
+            'NU' : 'Niue',
+            'NF' : 'Norfolk Island',
+            'MP' : 'Northern Mariana Islands',
+            'NO' : 'Norway',
+            'OM' : 'Oman',
+            'PK' : 'Pakistan',
+            'PW' : 'Palau',
+            'PS' : 'Palestinian Territory, Occupied',
+            'PA' : 'Panama',
+            'PG' : 'Papua New Guinea',
+            'PY' : 'Paraguay',
+            'PE' : 'Peru',
+            'PH' : 'Philippines',
+            'PN' : 'Pitcairn',
+            'PL' : 'Poland',
+            'PT' : 'Portugal',
+            'PR' : 'Puerto Rico',
+            'QA' : 'Qatar',
+            'RE' : 'Reunion',
+            'RO' : 'Romania',
+            'RU' : 'Russian Federation',
+            'RW' : 'Rwanda',
+            'BL' : 'Saint Barthelemy',
+            'SH' : 'Saint Helena',
+            'KN' : 'Saint Kitts And Nevis',
+            'LC' : 'Saint Lucia',
+            'MF' : 'Saint Martin',
+            'PM' : 'Saint Pierre And Miquelon',
+            'VC' : 'Saint Vincent And Grenadines',
+            'WS' : 'Samoa',
+            'SM' : 'San Marino',
+            'ST' : 'Sao Tome And Principe',
+            'SA' : 'Saudi Arabia',
+            'SN' : 'Senegal',
+            'RS' : 'Serbia',
+            'SC' : 'Seychelles',
+            'SL' : 'Sierra Leone',
+            'SG' : 'Singapore',
+            'SK' : 'Slovakia',
+            'SI' : 'Slovenia',
+            'SB' : 'Solomon Islands',
+            'SO' : 'Somalia',
+            'ZA' : 'South Africa',
+            'GS' : 'South Georgia And Sandwich Isl.',
+            'ES' : 'Spain',
+            'LK' : 'Sri Lanka',
+            'SD' : 'Sudan',
+            'SR' : 'Suriname',
+            'SJ' : 'Svalbard And Jan Mayen',
+            'SZ' : 'Swaziland',
+            'SE' : 'Sweden',
+            'CH' : 'Switzerland',
+            'SY' : 'Syrian Arab Republic',
+            'TW' : 'Taiwan',
+            'TJ' : 'Tajikistan',
+            'TZ' : 'Tanzania',
+            'TH' : 'Thailand',
+            'TL' : 'Timor-Leste',
+            'TG' : 'Togo',
+            'TK' : 'Tokelau',
+            'TO' : 'Tonga',
+            'TT' : 'Trinidad And Tobago',
+            'TN' : 'Tunisia',
+            'TR' : 'Turkey',
+            'TM' : 'Turkmenistan',
+            'TC' : 'Turks And Caicos Islands',
+            'TV' : 'Tuvalu',
+            'UG' : 'Uganda',
+            'UA' : 'Ukraine',
+            'AE' : 'United Arab Emirates',
+            'GB' : 'United Kingdom',
+            'US' : 'United States',
+            'UM' : 'United States Outlying Islands',
+            'UY' : 'Uruguay',
+            'UZ' : 'Uzbekistan',
+            'VU' : 'Vanuatu',
+            'VE' : 'Venezuela',
+            'VN' : 'Viet Nam',
+            'VG' : 'Virgin Islands, British',
+            'VI' : 'Virgin Islands, U.S.',
+            'WF' : 'Wallis And Futuna',
+            'EH' : 'Western Sahara',
+            'YE' : 'Yemen',
+            'ZM' : 'Zambia',
+            'ZW' : 'Zimbabwe'
+        };
 
-.controller("AddressConverterController", ["$scope", "$timeout", function($scope, $timeout) {
-      $scope.convert = function() {
-          $scope.bthaddress = "";
-          if ($scope.btcaddress) {
-              try {
-                var decoded = bitcoin.address.fromBase58Check($scope.btcaddress);
-                var version = decoded['version']
-                switch (version) {
-                  case 0:
-                    $scope.message = "Your BTH P2PKH Address";
-                    version = 25;
-                    break;
-                  case 25:
-                    $scope.message = "Your BTC P2PKH Address";
-                    version = 0;
-                    break;
-                  case 5:
-                    $scope.message = "Your BTH P2SH Address";
-                    version = 40;
-                    break;
-                  case 40:
-                    $scope.message = "Your BTC P2SH Address";
-                    version = 5;
-                    break;
-                  default:
-                    break;
+        $scope.getCountryName = function(countryCode) {
+            if (isoCountries.hasOwnProperty(countryCode)) {
+                return isoCountries[countryCode];
+            } else {
+                return countryCode;
+            }
+        };
+
+        $scope.fromNow = function(date) {
+            return moment(date).fromNow();
+        };
+
+        var map = new Datamap({
+            element: document.getElementById("map"),
+            geographyConfig: {
+                popupOnHover: true,
+                highlightOnHover: true,
+                highlightBorderColor: '#FFC345',
+                highlightFillColor: function(geo) {
+                    return '#FFC345';
                 }
-                if (version) $scope.bthaddress = bitcoin.address.toBase58Check(decoded['hash'], version);
-                else $scope.message = "Invalid BTC address. Please type a valid BTC address."
-              }
-              catch(e) {
-                  $scope.message = "Invalid BTC address. Please type a valid BTC address."
-              }
-          }
-      };
-      $scope.$watch("btcaddress", function() {
-          if (!$scope.btcaddress) {
-              $scope.message = "";
-              $scope.bthaddress = "";
-          }
-      });
+            },
+            fills: {
+                defaultFill: '#DDDDDD',
+                'node': '#1D2631',
+                'node-fill': '#000000',
+            }
+        });
+
+        $.get("/all").then(function(data) {
+                $timeout(function() {
+                    if (data.nodes) {
+                        data.nodes = data.nodes.map(function(node) {
+                            node.latitude = node.callingip_lat
+                            node.longitude = node.callingip_long
+                            node.radius = 10
+                            node.fillKey = 'node'
+                            return node;
+                        })
+                        $scope.nodes = data.nodes;
+                        map.bubbles(data.nodes, {
+                            highlightBorderWidth: 2,
+                            highlightFillColor: function(geo) {
+                                return '#FFC345';
+                            },
+                            highlightBorderColor: '#FFC345',
+                            popupTemplate: function(geo, data) {
+                                return '<div class="hoverinfo"> ' + data.callingip_city + "," + data.callingip_region
+                            }
+                        });
+                    }
+                })
+        })
 }])
--- public/assets/js/app/init.js
+++ public/assets/js/app/init.js
@@ -14,27 +14,6 @@ config(['$locationProvider', '$interpolateProvider', '$routeProvider', function(
       .when("/", {
           templateUrl : "/views/pages/page-landing.html"
       })
-      .when("/faq", {
-          templateUrl : "/views/pages/page-faq.html"
-      })
-      .when("/ourteam", {
-          templateUrl : "/views/pages/page-ourteam.html"
-      })
-      .when("/richlist", {
-          templateUrl : "/views/pages/page-richlist.html"
-      })
-      .when("/resources", {
-          templateUrl : "/views/pages/page-resources.html"
-      })
-      .when("/shirts", {
-          templateUrl : "/views/pages/page-shirts.html"
-      })
-      .when("/address-converter", {
-          templateUrl : "/views/pages/page-addressconverter.html"
-      })
-      .when("/key-converter", {
-          templateUrl : "/views/pages/page-keyconverter.html"
-      })
 }])
 
 .run(['$rootScope', '$anchorScroll', function($rootScope, $anchorScroll) {
