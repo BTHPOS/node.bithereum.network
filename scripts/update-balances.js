b32c69a474f75d6d3b9759c9f326d3a65096c409
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-07-22 16:53:15 -0400

    Turned balance update script into a periodic running script

---
+++ scripts/update-balances.js
@@ -0,0 +1,54 @@
+// HTTP Requests
+var got = require('got');
+
+// Database connection
+let mysql      = require('mysql');
+let pool = mysql.createPool({
+    connectionLimit : 10,
+    host     : 'chaindata.bithereum.network',
+    user     : 'root',
+    password : 'btcinnovations1923!',
+    database : 'chaindata'
+});
+
+let query = function(query, data, callback) {
+    pool.getConnection(function(err, connection) {
+         if (!err) {
+            connection.query(query, data, function (error, results, fields) {
+                connection.release();
+                if (typeof callback == "function") callback(error, results, fields);
+              });
+         }
+    });
+};
+
+setInterval(function() {
+
+      query("SELECT * FROM bth_nodes", [], function(err, rows) {
+            if (!err) {
+                for (var row in rows) {
+
+                    var _address = rows[row].bthaddress
+                    var _url = "http://insight.bithereum.network/insight-api/addr/"+_address+"/?noTxList=1"
+
+                    var getBalance = function(address, url) {
+                        got(url).then(function(data) {
+                            if (data.body)  {
+                                  var balance = 0;
+                                  try {
+                                      var response = JSON.parse(data.body);
+                                      balance = response.balance;
+                                  } catch(e) {}
+                                  console.log(address,"-->",balance);
+                                  query("UPDATE bth_nodes SET balance = '"+balance+"', balance_updated_on=NOW() WHERE `bthaddress` = '"+address+"'")
+                              }
+                        });
+                    }
+
+                    if (_address) getBalance(_address, _url);
+
+                }
+            }
+      });
+
+  },  30 * (60 * 1000) );
