84f2439fe87632b7ec77eb13a0e1de0afab3320d
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-08-05 16:48:23 -0400

    Fixing nodes

--- server.js
+++ server.js
@@ -204,9 +204,8 @@ var initialization = async function() {
                     data.nodetool_version = (params.nodetool_version || "")
                     data.nodetool_os = (params.nodetool_operatingsystem || "")
                     data.nodetool_identifier = (params.nodetool_identifier || "")
-                    console.log("-1", node);
                     (function(_data) {
-
+                      console.log( "SELECT * FROM bth_nodes WHERE ipid = '"+_data.ipid+"'" );
                         query("SELECT * FROM bth_nodes WHERE ipid = '"+_data.ipid+"'", {}, function(err, rows) {
                             console.log("0", arguments);
                             if (!err && rows.length > 0) {
