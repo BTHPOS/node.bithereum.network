2d71aea76876cbe4ed8efaa6f0b9782be7ae6582
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-09-03 18:52:21 -0400

    Testing query

--- server.js
+++ server.js
@@ -206,13 +206,13 @@ var initialization = async function() {
 
 
                     var addUpdateNodes = function(_data, highest_shares) {
-                        var nodeQueryParams = {
-                            reportedip: _data.reportedip,
-                            rpcport: _data.rpcport,
-                            p2pport: _data.p2pport,
-                            bthaddress: _data.bthaddress
-                        };
-                        query("SELECT * FROM bth_nodes WHERE ?", nodeQueryParams, function(err, rows) {
+                        var nodeQueryParams = [
+                            "reportedip=" + _data.reportedip,
+                            "rpcport=" + _data.rpcport,
+                            "p2pport=" + _data.p2pport,
+                            "bthaddress=" + _data.bthaddress
+                        ];
+                        query("SELECT * FROM bth_nodes WHERE " + nodeQueryParams.join(" AND "), {}, function(err, rows) {
                             if (!err && rows.length > 0) {
 
                                 let entry = rows[0]
